35a96796d02a8a560269c3c170224e0f3193f30d
Emil <120244710+Pragonna@users.noreply.github.com>
2023-04-26 16:20:40 +0400

    Add files via upload
    Use without keyboard - v

---
+++ Calculator_04/Calculator_04/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Calculator_04
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button0 = new Button();
+            button_AllClear = new Button();
+            button_Dot = new Button();
+            button_Equal = new Button();
+            button_plus = new Button();
+            button_minus = new Button();
+            button_Multiplication = new Button();
+            button_Divide = new Button();
+            textBox_main = new TextBox();
+            button_percent = new Button();
+            button_postitiveAndNegative = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(57, 262);
+            button1.Name = "button1";
+            button1.Size = new Size(47, 44);
+            button1.TabIndex = 0;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(110, 262);
+            button2.Name = "button2";
+            button2.Size = new Size(47, 44);
+            button2.TabIndex = 1;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(163, 262);
+            button3.Name = "button3";
+            button3.Size = new Size(47, 44);
+            button3.TabIndex = 2;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(57, 212);
+            button4.Name = "button4";
+            button4.Size = new Size(47, 44);
+            button4.TabIndex = 3;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(110, 212);
+            button5.Name = "button5";
+            button5.Size = new Size(47, 44);
+            button5.TabIndex = 4;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(163, 212);
+            button6.Name = "button6";
+            button6.Size = new Size(47, 44);
+            button6.TabIndex = 5;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(57, 162);
+            button7.Name = "button7";
+            button7.Size = new Size(47, 44);
+            button7.TabIndex = 6;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(110, 162);
+            button8.Name = "button8";
+            button8.Size = new Size(47, 44);
+            button8.TabIndex = 7;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(163, 162);
+            button9.Name = "button9";
+            button9.Size = new Size(47, 44);
+            button9.TabIndex = 8;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button0
+            // 
+            button0.Location = new Point(57, 312);
+            button0.Name = "button0";
+            button0.Size = new Size(100, 44);
+            button0.TabIndex = 9;
+            button0.Text = "0";
+            button0.UseVisualStyleBackColor = true;
+            button0.Click += button0_Click;
+            // 
+            // button_AllClear
+            // 
+            button_AllClear.Location = new Point(57, 112);
+            button_AllClear.Name = "button_AllClear";
+            button_AllClear.Size = new Size(47, 44);
+            button_AllClear.TabIndex = 10;
+            button_AllClear.Text = "AC";
+            button_AllClear.UseVisualStyleBackColor = true;
+            button_AllClear.Click += button_AllClear_Click;
+            // 
+            // button_Dot
+            // 
+            button_Dot.Location = new Point(163, 312);
+            button_Dot.Name = "button_Dot";
+            button_Dot.Size = new Size(47, 44);
+            button_Dot.TabIndex = 11;
+            button_Dot.Text = ".";
+            button_Dot.UseVisualStyleBackColor = true;
+            button_Dot.Click += button_Dot_Click;
+            // 
+            // button_Equal
+            // 
+            button_Equal.Location = new Point(216, 312);
+            button_Equal.Name = "button_Equal";
+            button_Equal.Size = new Size(47, 44);
+            button_Equal.TabIndex = 12;
+            button_Equal.Text = "=";
+            button_Equal.UseVisualStyleBackColor = true;
+            button_Equal.Click += button_Equal_Click;
+            // 
+            // button_plus
+            // 
+            button_plus.Location = new Point(216, 262);
+            button_plus.Name = "button_plus";
+            button_plus.Size = new Size(47, 44);
+            button_plus.TabIndex = 13;
+            button_plus.Text = "+";
+            button_plus.UseVisualStyleBackColor = true;
+            button_plus.Click += button_plus_Click;
+            // 
+            // button_minus
+            // 
+            button_minus.Location = new Point(216, 212);
+            button_minus.Name = "button_minus";
+            button_minus.Size = new Size(47, 44);
+            button_minus.TabIndex = 14;
+            button_minus.Text = "-";
+            button_minus.UseVisualStyleBackColor = true;
+            button_minus.Click += button_minus_Click;
+            // 
+            // button_Multiplication
+            // 
+            button_Multiplication.Location = new Point(216, 162);
+            button_Multiplication.Name = "button_Multiplication";
+            button_Multiplication.Size = new Size(47, 44);
+            button_Multiplication.TabIndex = 15;
+            button_Multiplication.Text = "x";
+            button_Multiplication.UseVisualStyleBackColor = true;
+            button_Multiplication.Click += button_Multiplication_Click;
+            // 
+            // button_Divide
+            // 
+            button_Divide.Location = new Point(216, 112);
+            button_Divide.Name = "button_Divide";
+            button_Divide.Size = new Size(47, 44);
+            button_Divide.TabIndex = 16;
+            button_Divide.Text = "/";
+            button_Divide.UseVisualStyleBackColor = true;
+            button_Divide.Click += button_Divide_Click;
+            // 
+            // textBox_main
+            // 
+            textBox_main.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox_main.Location = new Point(57, 67);
+            textBox_main.MaxLength = 14;
+            textBox_main.Name = "textBox_main";
+            textBox_main.Size = new Size(206, 39);
+            textBox_main.TabIndex = 17;
+            textBox_main.TextAlign = HorizontalAlignment.Right;
+            textBox_main.TextChanged += textBox_main_TextChanged;
+            // 
+            // button_percent
+            // 
+            button_percent.Enabled = false;
+            button_percent.ForeColor = SystemColors.ActiveCaptionText;
+            button_percent.Location = new Point(163, 112);
+            button_percent.Name = "button_percent";
+            button_percent.Size = new Size(47, 44);
+            button_percent.TabIndex = 18;
+            button_percent.Text = "%";
+            button_percent.UseVisualStyleBackColor = true;
+            button_percent.Click += button_percent_Click;
+            // 
+            // button_postitiveAndNegative
+            // 
+            button_postitiveAndNegative.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            button_postitiveAndNegative.Location = new Point(110, 112);
+            button_postitiveAndNegative.Name = "button_postitiveAndNegative";
+            button_postitiveAndNegative.Size = new Size(47, 44);
+            button_postitiveAndNegative.TabIndex = 19;
+            button_postitiveAndNegative.Text = "+/-";
+            button_postitiveAndNegative.UseVisualStyleBackColor = true;
+            button_postitiveAndNegative.Click += button_PostitiveAndNegative_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(333, 383);
+            Controls.Add(button_postitiveAndNegative);
+            Controls.Add(button_percent);
+            Controls.Add(textBox_main);
+            Controls.Add(button_Divide);
+            Controls.Add(button_Multiplication);
+            Controls.Add(button_minus);
+            Controls.Add(button_plus);
+            Controls.Add(button_Equal);
+            Controls.Add(button_Dot);
+            Controls.Add(button_AllClear);
+            Controls.Add(button0);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button0;
+        private Button button_AllClear;
+        private Button button_Dot;
+        private Button button_Equal;
+        private Button button_plus;
+        private Button button_minus;
+        private Button button_Multiplication;
+        private Button button_Divide;
+        private TextBox textBox_main;
+        private Button button_percent;
+        private Button button_postitiveAndNegative;
+    }
+}
---
+++ Calculator_04/Calculator_04/Form1.cs
@@ -0,0 +1,299 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Calculator_04
+{
+    public partial class Form1 : Form
+    {
+        decimal result = 0;
+        List<decimal> num = new List<decimal>();
+        int click = 0;
+        char symbol = default;
+        char tempSymbol = default;
+        public Form1()
+        {
+            InitializeComponent();
+            textBox_main.Text = "0";
+        }
+
+
+        private void textBox_main_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        #region Functions method
+        private void Divide()
+        {
+            click++;
+            if (result == 0)
+            {
+                num.Add(decimal.Parse(textBox_main.Text));
+                result = num[0];
+            }
+            else
+            {
+                num.Add(decimal.Parse(textBox_main.Text));
+                result /= num[num.Count - 1];
+                textBox_main.Text = result.ToString();
+            }
+        }
+        private void Multiplication()
+        {
+            click++;
+            if (result == 0)
+            {
+                num.Add(decimal.Parse(textBox_main.Text));
+                result = num[0];
+            }
+            else
+            {
+                num.Add(decimal.Parse(textBox_main.Text));
+                result *= num[num.Count - 1];
+                textBox_main.Text = result.ToString();
+            }
+        }
+        private void Total()
+        {
+            click++;
+            if (result == 0)
+            {
+                num.Add(decimal.Parse(textBox_main.Text));
+                result = num[0];
+            }
+            else
+            {
+                num.Add(decimal.Parse(textBox_main.Text));
+                result += num[num.Count - 1];
+                textBox_main.Text = result.ToString();
+            }
+        }
+        private void Minus()
+        {
+            click++;
+            if (result == 0)
+            {
+                num.Add(decimal.Parse(textBox_main.Text));
+                result = num[0];
+            }
+            else
+            {
+                num.Add(decimal.Parse(textBox_main.Text));
+                result -= num[num.Count - 1];
+                textBox_main.Text = result.ToString();
+            }
+        }
+
+
+        #endregion
+        #region If is or different Zero for number moving
+        private void IfTextBoxIsZeroValue()
+        {
+            switch (textBox_main.Text)
+            {
+                case "0":
+                    textBox_main.Clear();
+                    break;
+            }
+            IfResultDifferentZero();
+        }
+        private void IfResultDifferentZero()
+        {
+            if (result != 0 && click > 0)
+            {
+                textBox_main.Clear();
+                click = 0;
+            }
+        }
+        #endregion
+        #region Button Number
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IfTextBoxIsZeroValue();
+            textBox_main.Text += "1";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            IfTextBoxIsZeroValue();
+            textBox_main.Text += "2";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            IfTextBoxIsZeroValue();
+            textBox_main.Text += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            IfTextBoxIsZeroValue();
+            textBox_main.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            IfTextBoxIsZeroValue();
+            textBox_main.Text += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            IfTextBoxIsZeroValue();
+            textBox_main.Text += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            IfTextBoxIsZeroValue();
+            textBox_main.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            IfTextBoxIsZeroValue();
+            textBox_main.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            IfTextBoxIsZeroValue();
+            textBox_main.Text += "9";
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            IfTextBoxIsZeroValue();
+            textBox_main.Text += "0";
+        }
+
+        private void button_Dot_Click(object sender, EventArgs e)
+        {
+            textBox_main.Text += ".";
+        }
+        #endregion
+
+        private void button_AllClear_Click(object sender, EventArgs e)
+        {
+            textBox_main.Clear();
+            result = 0;
+        }
+
+        private void button_PostitiveAndNegative_Click(object sender, EventArgs e)
+        {
+            SymbolFiltered();
+            var txtMain = decimal.Parse(textBox_main.Text);
+            if (txtMain > 0)
+            {
+                textBox_main.Text = "-" + textBox_main.Text;
+            }
+            else if (txtMain < 0)
+            {
+                textBox_main.Text = textBox_main.Text.Remove(0, 1);
+            }
+            result = decimal.Parse(textBox_main.Text);
+
+        }
+
+        private void button_plus_Click(object sender, EventArgs e)
+        {
+            tempSymbol = symbol;
+            symbol = '+';
+            ProcessResult();
+        }
+
+        private void button_Equal_Click(object sender, EventArgs e)
+        {
+            SymbolFiltered();
+        }
+
+        private void SymbolFiltered()
+        {
+            switch (symbol)
+            {
+                case '+':
+                    Total();
+                    break;
+                case '-':
+                    Minus();
+                    break;
+                case 'x':
+                    Multiplication();
+                    break;
+                case '/':
+                    Divide();
+                    break;
+            }
+            symbol = default;
+        }
+
+        private void ProcessResult()
+        {
+            if (result != 0)
+            {
+                switch (tempSymbol)
+                {
+                    case '+':
+                        Total();
+                        break;
+                    case '-':
+                        Minus();
+                        break;
+                    case 'x':
+                        Multiplication();
+                        break;
+                    case '/':
+                        Divide();
+                        break;
+                }
+            }
+            else
+            {
+                switch (symbol)
+                {
+                    case '+':
+                        Total();
+                        break;
+                    case '-':
+                        Minus();
+                        break;
+                    case 'x':
+                        Multiplication();
+                        break;
+                    case '/':
+                        Divide();
+                        break;
+                }
+            }
+        }
+
+        private void button_minus_Click(object sender, EventArgs e)
+        {
+            tempSymbol = symbol;
+            symbol = '-';
+            ProcessResult();
+
+        }
+
+        private void button_Multiplication_Click(object sender, EventArgs e)
+        {
+            tempSymbol = symbol;
+            symbol = 'x';
+            ProcessResult();
+        }
+
+        private void button_Divide_Click(object sender, EventArgs e)
+        {
+            tempSymbol = symbol;
+            symbol = '/';
+            ProcessResult();
+        }
+
+        private void button_percent_Click(object sender, EventArgs e)
+        {
+            //tempSymbol = symbol;
+            //symbol = '%';
+            //ProcessResult();
+        }
+    }
+}
